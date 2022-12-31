780154771ab627dc28438dbc0996d4b42b22395c
ZAWADBINSHARIF <syedzawadbinsharif@gmail.com>
2022-12-31 11:41:45 +0600

    Build search bar event action

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function searchActionBar() {
+    const searchBarValue = document.querySelector('#searchProductName').value.toUpperCase()
+    const all_products_table_tbody = document.querySelector('#all-products-table').querySelector('tbody')
+    const products_table_tr = all_products_table_tbody.querySelectorAll('tr')
+
+    for (let i = 0; i < products_table_tr.length; i++) {
+        const products_table_td = products_table_tr[i].getElementsByTagName('td')[1]
+
+        if (products_table_td) {
+
+            const getProductName = products_table_td.textContent.toUpperCase()
+
+            if (getProductName.indexOf(searchBarValue) > -1) {
+                products_table_tr[i].style.display = ''
+            } else {
+                products_table_tr[i].style.display = 'none'
+            }
+
+        }
+    }
+
+}
